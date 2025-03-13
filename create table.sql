20ae017c34508f41b3f5e9a295c0c3654ffcb5db
Anastasia Kim <123845505+kimavit@users.noreply.github.com>
2025-03-13 21:47:39 +0300

    Update create table.sql

--- create table.sql
+++ create table.sql
@@ -1,13 +1,18 @@
 CREATE TABLE patient (
-    full_name VARCHAR(100),
-    sex CHAR (1),
+    full_name VARCHAR(100) NOT NULL,
+    sex CHAR(1) DEFAULT 'м',
     birth_date DATE,
-    oms_num BIGINT
-
+    oms_num BIGINT UNIQUE,
+    card_num INT,
+    area_num TINYINT, 
+    FOREIGN KEY (area_num) REFERENCES med_area(area_num)
 );
-INSERT INTO patients (full_name, sex, birth_date, oms_num)
-VALUES ('Скрипкина Надежда Константиновна', 'ж', '1965-06-23', 47327844534),
-('Чусов Виктор Петрович', 'мbooks_in_usebooks_in_use', '1978-09-01', 36327844534),
-('Калинина Юлия Васильевна', 'ж', '1995-03-15', 78327844534);
 
-SELECT*FROM patients
+INSERT INTO patients2 (full_name, sex, birth_date, oms_num, card_num)
+VALUES ('Скрипкина Надежда Константиновна', 'ж', '1965-06-23', 47327844534, 1677),
+('Чусов Виктор Петрович', 'м', '1978-09-01', 36327844534, 235),
+('Калинина Юлия Васильевна', 'ж', '1995-03-15', 78327844534, 3265),
+('Иванов Сергей Эдуардович', 'м', '1965-08-15', 3224584531, 2345),
+('Скрябин Евгений Дмитриевич', 'м', '1985-11-25', 45320544731, 2678),
+('быкова Светлана Ивановна', 'ж', '2001-12-16', 48324544531, 5623)
+;
